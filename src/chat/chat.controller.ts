c706997efb05bcbc35060a11603fef8ce8cdf1e2
vector-rc <fernando.rdcrz@gmail.com>
2022-08-29 13:38:36 -0500

    format code

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,12 +1,12 @@
-import { Body, Controller, Get, Post, Put, Query, HttpException, HttpStatus, Inject, Param } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ChatService } from './chat.service';
 
 @Controller('chat')
 export class ChatController {
-    constructor(private readonly chatService: ChatService) {}
+    constructor(private readonly chatService: ChatService) { }
 
     @Post()
-    async sendtextChat(@Body() data:any) {
+    async sendtextChat(@Body() data: any) {
         return await this.chatService.remove(data)
     }
 }
--- src/session/session.controller.ts
+++ src/session/session.controller.ts
@@ -1,34 +1,34 @@
-import { Body, Controller,  Post, Put, Query, HttpException, HttpStatus, Inject, Param } from '@nestjs/common';
+import { Body, Controller, Post, Put, HttpException, HttpStatus } from '@nestjs/common';
 import { SessionService } from './session.service';
 
 @Controller('session')
 export class SessionController {
-    constructor(private readonly sessionService:SessionService) {}
+    constructor(private readonly sessionService: SessionService) { }
 
     @Post()
-    async create(@Body() data:any){
-       const res=await this.sessionService.createClient(data)
-       if(!res.success){
-        throw new HttpException(res,HttpStatus.NOT_ACCEPTABLE)
-       }
-       return res
+    async create(@Body() data: any) {
+        const res = await this.sessionService.createClient(data)
+        if (!res.success) {
+            throw new HttpException(res, HttpStatus.NOT_ACCEPTABLE)
+        }
+        return res
     }
 
     @Put('start')
-    async restart(@Body() data:any){
-       const res=await this.sessionService.openClient(data)
-       if(!res.success){
-        throw new HttpException(res,HttpStatus.NOT_ACCEPTABLE)
-       }
-       return res
+    async restart(@Body() data: any) {
+        const res = await this.sessionService.openClient(data)
+        if (!res.success) {
+            throw new HttpException(res, HttpStatus.NOT_ACCEPTABLE)
+        }
+        return res
     }
 
     @Put('shutdown')
-    async close(@Body() data:any){
-       const res=await this.sessionService.closeClient(data)
-       if(!res.success){
-        throw new HttpException(res,HttpStatus.NOT_ACCEPTABLE)
-       }
-       return res
+    async close(@Body() data: any) {
+        const res = await this.sessionService.closeClient(data)
+        if (!res.success) {
+            throw new HttpException(res, HttpStatus.NOT_ACCEPTABLE)
+        }
+        return res
     }
 }
