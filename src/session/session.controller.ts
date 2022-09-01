89152cefe06a3dda4c36e8bff7e9a76fdb7ad8b1
vector-rc <fernando.rdcrz@gmail.com>
2022-09-01 18:05:38 -0500

    added new endpoints for create new sessions and get qr codes

--- src/session/session.controller.ts
+++ src/session/session.controller.ts
@@ -1,29 +1,60 @@
-import { Body, Controller, Post, Put, HttpException, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Put, HttpException, HttpStatus, Get, Param } from '@nestjs/common';
 import { SessionService } from './session.service';
 
 @Controller('session')
 export class SessionController {
-    constructor(private readonly sessionService: SessionService) { }
+    constructor(private sessionService: SessionService) { }
 
-    @Post()
-    async create(@Body() data: any) {
-        const res = await this.sessionService.createClient(data)
-        if (!res.success) {
-            throw new HttpException(res, HttpStatus.NOT_ACCEPTABLE)
-        }
-        return res
+    @Get()
+    async findAll() {
+        return this.sessionService.findAll()
     }
 
-    @Put('start')
+    @Get('qrCode')
+    async getQr() {
+        return this.sessionService.getQrCode()
+    }
+
+    @Get('id/:sessionId')
+    findById(@Param('sessionId') sessionId: string) {
+        const session = this.sessionService.findById(sessionId)
+        if(!session){
+            throw new HttpException({message:'No existe session con este id'}, HttpStatus.NOT_ACCEPTABLE)
+        }
+        return {
+            id: session.id,
+            wid: session.wid,
+            webhook: session.webhook,
+            state: session.state
+        }
+    }
+
+    @Get('wid/:wid')
+    findByWid(@Param('wid') wid: string) {
+        const session = this.sessionService.findByWid(wid)
+        return {
+            id: session.id,
+            wid: session.wid,
+            webhook: session.webhook,
+            state: session.state
+        }
+    }
+
+    // @Put('forceRefreshQrCode')
+    // async forceRefreshQrCode() {
+    //     return this.sessionService.getQrCode()
+    // }
+
+    @Put('start') //esto inicia o reinicia la instancia de chrome
     async restart(@Body() data: any) {
-        const res = await this.sessionService.openClient(data)
+        const res = await this.sessionService.startClient(data)
         if (!res.success) {
             throw new HttpException(res, HttpStatus.NOT_ACCEPTABLE)
         }
         return res
     }
 
-    @Put('shutdown')
+    @Put('shutdown') //esto cierra la instancia de chrome
     async close(@Body() data: any) {
         const res = await this.sessionService.closeClient(data)
         if (!res.success) {
@@ -31,4 +62,23 @@ export class SessionController {
         }
         return res
     }
+
+    @Put('logout')
+    async logout(@Body() data: any) {// esto cierra la session o desvincula el dispositivo
+        const res = await this.sessionService.logout(data)
+        if (!res.success) {
+            throw new HttpException(res, HttpStatus.NOT_ACCEPTABLE)
+        }
+        return res
+    }
+
+    @Put('/webhook')
+    async changeWebhook(@Body() data: any) {
+        const res = this.sessionService.updateWebhook(data.id, data.webhook)
+        if (!res) {
+            throw new HttpException({ message: 'Error al cambiar el webhook', success: false }, HttpStatus.NOT_ACCEPTABLE)
+        }
+        return { message: 'Webhook actualizado exitosamente', success: true }
+    }
+
 }
--- src/session/session.service.ts
+++ src/session/session.service.ts
@@ -1,42 +1,54 @@
-import {Injectable, OnModuleDestroy } from '@nestjs/common'
-import { Ack, create, Message, MessageType, Whatsapp } from '@wppconnect-team/wppconnect';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import {
+    Ack,
+    create,
+    Message,
+    MessageType,
+    StatusFind,
+    Whatsapp,
+} from '@wppconnect-team/wppconnect';
 import * as fs from 'node:fs';
 import axios, { Axios } from 'axios';
-import * as https from 'node:https'
+import * as https from 'node:https';
+import { ScrapQrcode } from '@wppconnect-team/wppconnect/dist/api/model/qrcode';
 
-interface WhatsappClient {
-    id?: string,
-    wid: string,
-    client?: Whatsapp,
-    webhook: string
+interface WhatsappSession {
+    id?: string;
+    wid?: string;
+    whatsapp?: Whatsapp;
+    webhook?: string;
+    state?: StatusFind;
 }
 
 @Injectable()
 export class SessionService implements OnModuleDestroy {
-
     private axios: Axios;
-    private clients: { [key: string]: WhatsappClient } = {}
-    private availableNumbers: string[] = ['51938432015']
+    private sessions: WhatsappSession[] = [];
+    private availableNumbers: string[] = ['51938432015'];
+    private qrCodeSessions: Array<ScrapQrcode & { sessionId: string }> = [];
+    private onMessageEvents: { [key: string]: { dispose: () => void } } = {}
+    private onAckEvents: { [key: string]: { dispose: () => void } } = {}
 
     constructor() {
+        console.log('NEW SESSION SERVICE');
+
         if (fs.existsSync('whatsapp-clients.json')) {
-            const clients = fs.readFileSync('whatsapp-clients.json', 'utf-8')
-            this.init(JSON.parse(clients))
+            this.sessions = JSON.parse(fs.readFileSync('whatsapp-clients.json', 'utf-8'))
+            this.init();
         }
 
         if (!fs.existsSync('available_numbers.json')) {
-            this.updateAvailableNumbers()
+            this.updateAvailableNumbers();
         }
-
 
         this.axios = axios.create({
             httpsAgent: new https.Agent({ rejectUnauthorized: false }),
         });
     }
 
-    private async init(clients: WhatsappClient[]) {
-        for (const client of clients) {
-            await this.createClient(client, false)
+    private async init() {
+        for (const session of this.sessions) {
+            await this.createClient(session.id);
         }
     }
 
@@ -45,155 +57,287 @@ export class SessionService implements OnModuleDestroy {
     }
 
     updateAvailableNumbers() {
-        fs.writeFileSync('available_numbers.json', JSON.stringify(this.availableNumbers))
+        fs.writeFileSync('available_numbers.json', JSON.stringify(this.availableNumbers));
     }
 
-    async createClient(data: WhatsappClient, addToFile = true): Promise<{ data: any, message: string, success: boolean }> {
+    findAll() {
+        return this.sessions.map(({ id, wid, webhook, state }) => ({ id, wid, webhook, state }));
+    }
 
-        if (Object.values(this.clients).length === 5) {
-            return { data: [], success: false, message: 'No se puede crear mas de 5 sessiones' }
+    updateWebhook(id: string, newWebhook: string) {
+        const sessionIndex = this.sessions.findIndex((s) => s.id === id);
+
+        if (sessionIndex === -1) return false
+
+        if (!this.sessions[sessionIndex].whatsapp) return false
+
+        this.sessions[sessionIndex].webhook = newWebhook;
+        this.updateClientsFileRepo();
+
+        try {
+            this.registerOnAckCallback(id);
+            this.registerOnMessageCallback(id);
+            return true;
+        } catch (e) {
+            console.log('WEBHOOK ERROR', e);
+            return false;
         }
-        const session = data.id ?? this.uid()
-        let qrCodeBase64: string
-        const client = await create({
-            session,//test
+    }
+
+    findById(sessionId: string) {
+        return this.sessions.find((s) => s.id === sessionId);
+    }
+
+    private async createClient(id: string = null): Promise<{ data: any; message: string; success: boolean }> {
+        if (this.sessions.length === 5) {
+            return { data: [], success: false, message: 'No se puede crear mas de 5 sessiones' };
+        }
+        const sessionId = id ?? this.uid();
+        const session = this.findById(sessionId);
+
+        if (!session) {
+            this.sessions.push({ id: sessionId });
+            this.updateClientsFileRepo()
+        }
+
+        const whatsapp = await create({
+            session: sessionId,
             catchQR: (base64Qrimg, asciiQR, attempts, urlCode) => {
-                // console.log('Number of attempts to read the qrcode: ', attempts);
-                console.log('Terminal qrcode: ', asciiQR);
-                qrCodeBase64 = base64Qrimg
-                // console.log('base64 image string qrcode: ', base64Qrimg);
-                // console.log('urlCode (data-ref): ', urlCode);
+                console.log(`Waiting for QRCode Scan (Attempt ${attempts})...`, (new Date()).toLocaleString());
+
+                const qrCodeIndex = this.qrCodeSessions.findIndex((qrc) => qrc.sessionId === sessionId);
+                if (qrCodeIndex === -1) {
+                    this.qrCodeSessions.push({ base64Image: base64Qrimg, urlCode, sessionId });
+                } else {
+                    this.qrCodeSessions[qrCodeIndex] = { base64Image: base64Qrimg, urlCode, sessionId };
+                }
             },
-            statusFind: (statusSession, session) => {
-                console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken
-                //Create session wss return "serverClose" case server for close
-                console.log('Session name: ', session);
+
+            statusFind: async (statusSession, session) => {
+                console.log('Session name: ', session, '\nStatus Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken
+
+                const sessionIndex = this.sessions.findIndex((s) => s.id === sessionId);
+                this.sessions[sessionIndex].state = statusSession as StatusFind;
+
+                if (['autocloseCalled', 'notLogged', 'browserClose', 'serverClose', 'qrReadError', 'desconnectedMobile'].includes(statusSession)) {
+                    const qrCodeIndex = this.qrCodeSessions.findIndex((qrc) => qrc.sessionId === sessionId);
+                    if (qrCodeIndex !== -1) {
+                        this.qrCodeSessions.splice(qrCodeIndex, 1);
+                    }
+                    this.sessions[sessionIndex].wid = null;
+                    this.sessions[sessionIndex].webhook = null;
+                }
+
+                if ([StatusFind.inChat, StatusFind.isLogged].includes(statusSession as StatusFind)) {
+                    const qrCodeIndex = this.qrCodeSessions.findIndex((qrc) => qrc.sessionId === sessionId);
+                    if (qrCodeIndex !== -1) {
+                        this.qrCodeSessions.splice(qrCodeIndex, 1);
+                    }
+                }
+
+                if (statusSession === 'inChat') {
+                    if (!this.sessions[sessionIndex].whatsapp) {
+
+                        const interval = setInterval(async () => {
+                            if (this.sessions[sessionIndex].whatsapp) {
+                                this.sessions[sessionIndex].wid = (
+                                    await this.sessions[sessionIndex].whatsapp.getWid()
+                                ).split('@')[0];
+                                this.updateWebhook(sessionId, this.sessions[sessionIndex].webhook)
+                                this.updateClientsFileRepo()
+                                clearInterval(interval);
+                            }
+                        }, 1000);
+                    } else {
+                        this.sessions[sessionIndex].wid = (
+                            await this.sessions[sessionIndex].whatsapp.getWid()
+                        ).split('@')[0];
+                        this.updateWebhook(sessionId, this.sessions[sessionIndex].webhook)
+                    }
+
+
+                }
             },
-            headless: true, // Headless chrome
-            devtools: false, // Open devtools by default
-            useChrome: true, // If false will use Chromium instance
-            debug: false, // Opens a debug session
-            logQR: true, // Logs QR automatically in terminal
-            browserWS: '', // If u want to use browserWSEndpoint
-            browserArgs: [''], // Parameters to be added into the chrome browser instance
-            puppeteerOptions: {}, // Will be passed to puppeteer.launch
+            puppeteerOptions: {}, // is nessessary for mutiple sessions
+            autoClose: 300000,
+            logQR: false,
             disableWelcome: true, // Option to disable the welcoming message which appears in the beginning
-            updatesLog: true, // Logs info updates automatically in terminal
-            autoClose: 60000, // Automatically closes the wppconnect only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
             tokenStore: 'file', // Define how work with tokens, that can be a custom interface
             folderNameToken: './tokens', //folder name when saving tokens
         });
 
+        const sessionIndex = this.sessions.findIndex((s) => s.id === sessionId);
+        this.sessions[sessionIndex].whatsapp = whatsapp;
 
-        const [wid] = (await client.getWid()).split('@')
+        return { data: { id: sessionId }, success: true, message: 'Cliente creado exitosamente' };
+    }
 
-        console.log('WID---', wid);
+    registerOnMessageCallback(sessionId: string) {
+        const whatsappSession: WhatsappSession = this.findById(sessionId)
+        const { wid, whatsapp, webhook } = whatsappSession;
+        if (this.onMessageEvents[sessionId]) {
+            this.onMessageEvents[sessionId].dispose()
+        }
 
-
-        client.onAnyMessage(async (message: Message) => {
-            console.log('CALLBACK DE ' + wid, 'SESSION NAME ' + session);
+        this.onMessageEvents[sessionId] = whatsapp.onMessage(async (message: Message) => {
+            console.log('CALLBACK DE ' + wid);
             if (message.isGroupMsg) {
                 console.log('Mensaje de grupo', message.body);
-                return
+                return;
             }
 
             if (message.from === 'status@broadcast') {
                 console.log('STATUS IGNORED', message.sender);
-                return
-            }
-
-            if (message.self === 'out') {
-                console.log('MENSAJE ENVIADO =>', message.body);
+                return;
             }
 
             if (message.type === MessageType.STICKER) {
                 console.log('STIKER');
-                return
+                return;
             }
 
             if (message.type === MessageType.E2E_NOTIFICATION) {
                 console.log(message.type);
-                return
+                return;
             }
 
             if (message.self === 'in' && wid === message.to.split('@')[0]) {
                 console.log('MENSAJE RECIBIDO', message);
 
-                const clientNumber = message.from.split('@')[0]
+                const clientNumber = message.from.split('@')[0];
 
                 if (this.availableNumbers.includes(clientNumber)) {
-                    await this.axios.post(data.webhook, { sid: message.id.split('_')[2], ...message });
-                    return
+                    try {
+                        await this.axios.post(webhook, { sid: message.id.split('_')[2], ...message });
+
+                    } catch (error) {
+                        console.log('ERROR EN EL WEBHOOK POST ', error);
+                    }
+                    return;
                 }
 
                 if (message.body === 'join wpp-onbotgo') {
-                    this.availableNumbers.push(clientNumber)
-                    this.updateAvailableNumbers()
-                    client.sendText(clientNumber, 'Te has unido a wpp-onbotgo')
+                    this.availableNumbers.push(clientNumber);
+                    this.updateAvailableNumbers();
+                    whatsapp.sendText(clientNumber, 'Te has unido a wpp-onbotgo');
                 }
             }
         });
+    }
 
-        client.onAck((ack: Ack) => {
+    registerOnAckCallback(sessionId: string) {
+        const whatsappSession: WhatsappSession = this.findById(sessionId)
+        const { wid, whatsapp, webhook } = whatsappSession;
+
+        if (this.onAckEvents[sessionId]) {
+            this.onAckEvents[sessionId].dispose()
+        }
+
+        this.onAckEvents[sessionId] = whatsapp.onAck(async (ack: Ack) => {
             if (ack.self === 'out' && wid === ack.from.split('@')[0]) {
-                console.log('ACK ' + wid,  ack);
-                this.axios.put(data.webhook, { sid: ack.id.id, ...ack });
+                console.log('ACK ' + wid, ack);
+                try {
+                    await this.axios.put(webhook, { sid: ack.id.id, ...ack });
+                } catch (error) {
+                    console.log('ERROR EN EL WEBHOOK PUT ', error);
+                }
             }
         });
-
-        //client.tokenStore.removeToken('test')
-
-        this.clients[session] = { id: session, client, webhook: data.webhook, wid }
-
-        if (addToFile) {
-            const saveClients = Object.values(this.clients).map(c => ({ id: c.id, wid: c.wid, webhook: c.webhook }))
-            fs.writeFileSync('whatsapp-clients.json', JSON.stringify(saveClients))
-        }
-        return { data: { id: session, qrCodeBase64 }, success: true, message: 'Session creada exitosamente' }
     }
 
-    async closeClient(data: WhatsappClient): Promise<{ data: any, message: string, success: boolean }> {
-        let client: WhatsappClient
+    updateClientsFileRepo() {
+        const clientsToSave = this.sessions.map(({ id, wid, webhook }) => ({ id, wid, webhook }));
+        fs.writeFileSync('whatsapp-clients.json', JSON.stringify(clientsToSave));
+    }
 
-        if (data.wid) client = this.findClientByWid(data.wid)
-        if (data.id) client = this.findClientById(data.id)
+    async logout(data: WhatsappSession): Promise<{ data: any; message: string; success: boolean }> {
+        let session: WhatsappSession;
 
-        if (!client) {
-            return { data: null, message: 'El cliente no existe o aun no se ha iniciado', success: false }
+        if (data.wid) session = this.findByWid(data.wid);
+        if (data.id) session = this.findById(data.id);
+
+
+        if (!session) {
+            return { data: null, message: 'La session no existe o aun no se ha iniciado', success: false };
         }
 
-        const itClosed = await client.client.close()
-        return { data: itClosed, message: 'Cliente cerrado exitosamente', success: true }
-
-    }
-
-    async openClient(data: WhatsappClient): Promise<{ data: any, message: string, success: boolean }> {
-        let client: WhatsappClient
-
-        if (data.wid) client = this.findClientByWid(data.wid)
-        if (!data.id) client = this.findClientById(data.id)
-
-        if (!client) {
-            return { data: null, message: 'El cliente no existe', success: false }
+        if (!session.whatsapp) {
+            return { data: null, message: 'La session no existe o aun no se ha iniciado', success: false };
         }
 
-        if (data.webhook) client.webhook = data.webhook
+        const itLogout = await session.whatsapp.logout();
+        const sessionIndex = this.sessions.findIndex((s) => s.id === session.id);
+        this.sessions[sessionIndex].wid = null;
 
-        return await this.createClient(client, false)
-
+        return { data: itLogout, message: 'Session cerrada exitosamente', success: true };
     }
 
-    findClientById(clientId: string) {
-        return this.clients[clientId]
+    async getQrCode(): Promise<{ data: any; message: string; success: boolean }> {
+        let qrCode = this.qrCodeSessions[0];
+
+        if (qrCode) return { data: qrCode, message: 'QR', success: true };
+        const session = this.sessions.find((s) => ['autocloseCalled', 'notLogged', 'qrReadError'].includes(s.state));
+
+        if (session) this.createClient(session.id);
+        else this.createClient();
+
+        await new Promise((resolve) => {
+            const interval = setInterval(() => {
+                if (this.qrCodeSessions[0]) {
+                    qrCode = this.qrCodeSessions[0];
+                    clearInterval(interval);
+                    resolve(true);
+                }
+            }, 2000);
+        });
+
+        return { data: qrCode, message: 'QR', success: true };
     }
 
-    findClientByWid(wid: string) {//wid es el numero de whatsapp
-        return Object.values(this.clients).find(c => c.wid === wid)
+    async closeClient(data: WhatsappSession): Promise<{ data: any; message: string; success: boolean }> {
+        let session: WhatsappSession;
+
+        if (data.wid) session = this.findByWid(data.wid);
+        if (data.id) session = this.findById(data.id);
+
+        if (!session) {
+            return { data: null, message: 'La session no existe o aun no se ha iniciado', success: false };
+        }
+        if (session.state === 'browserClose') {
+            return { data: null, message: 'Esta session ya esta apagada', success: false };
+        }
+
+        const itClosed = await session.whatsapp.close();
+        return { data: itClosed, message: 'Session cerrada exitosamente', success: true };
+    }
+
+    async startClient(data: WhatsappSession): Promise<{ data: any; message: string; success: boolean }> {
+        let session: WhatsappSession;
+
+        if (data.wid) session = this.findByWid(data.wid);
+        if (data.id) session = this.findById(data.id);
+
+        if (!session) {
+            return { data: null, message: 'El sessione no existe o aun no se ha iniciado', success: false };
+        }
+
+        if (session.state === 'inChat') {
+            return { data: null, message: 'Esta session ya esta iniciada', success: false };
+        }
+
+        if (data.webhook) session.webhook = data.webhook;
+
+        return await this.createClient(session.id);
+    }
+
+    findByWid(wid: string) {
+        //wid es el numero de whatsapp
+        return this.sessions.find((s) => s.wid === wid);
     }
 
     async onModuleDestroy() {
-        for (const client of Object.values(this.clients)) {
-            await client.client.close()
+        for (const session of this.sessions) {
+            await session.whatsapp.close();
         }
     }
 }
