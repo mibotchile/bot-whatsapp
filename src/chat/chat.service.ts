4f66c6b4ec5e016eb7953e51aa23d68e0538d2ab
vector-rc <fernando.rdcrz@gmail.com>
2022-09-01 18:04:59 -0500

    rename methods

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -3,16 +3,16 @@ import { SessionService } from 'src/session/session.service';
 
 @Injectable()
 export class ChatService {
-   
+
     constructor(private sessionService: SessionService) {
     }
 
     async remove(data:{whatsappNumber:string,chatNumber:string}) {
-        const wppClient = this.sessionService.findClientByWid(data.whatsappNumber)
-        if(!wppClient){
+        const session = this.sessionService.findByWid(data.whatsappNumber)
+        if(!session){
             throw new HttpException({success:false,message:'No existe session con este numero'},HttpStatus.NOT_ACCEPTABLE)
         }
-        return await wppClient.client.deleteChat(data.chatNumber+'@c.us')
+        return await session.whatsapp.deleteChat(data.chatNumber+'@c.us')
     }
 
 }
--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,19 +1,18 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { forwardRef, HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common'
 import { SessionService } from 'src/session/session.service';
 
 @Injectable()
 export class MessageService {
 
-    constructor(private sessionService: SessionService) {
+    constructor(@Inject(forwardRef(()=>SessionService)) private sessionService: SessionService) {
     }
 
     async send(data: { from: string, to: string, body: string }) {
-        const wppClient = this.sessionService.findClientByWid(data.from)
-        if (!wppClient) {
+        const session = this.sessionService.findByWid(data.from)
+        if (!session) {
             throw new HttpException({ success: false, message: 'No existe session con este numero' }, HttpStatus.NOT_ACCEPTABLE)
         }
-        // const message:Message&{sid:string}=await wppClient.client.sendText(data.to, data.body) as any
-        const message = await wppClient.client.sendText(data.to, data.body)
+        const message = await session.whatsapp.sendText(data.to, data.body)
         return { sid: message.id.split('_')[2], ...message }
     }
 
