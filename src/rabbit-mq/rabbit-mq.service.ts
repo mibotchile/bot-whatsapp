6853fa035736d604219638a46845e1747190b26d
vector-rc <fernando.rdcrz@gmail.com>
2022-09-12 17:11:02 -0500

    added new module for rabbitmq

---
+++ src/rabbit-mq/rabbit-mq.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, OnModuleInit } from '@nestjs/common'
+import { Channel, connect, Connection } from 'amqplib'
+
+@Injectable()
+export class RabbitMQService implements OnModuleInit {
+    channel: Channel
+    connection: Connection
+
+    async onModuleInit() {
+        console.log('SE INICIO EL RABBIT SERVICE')
+        this.connection = await connect(process.env.RABBIT_URL, 'heartbeat=60')
+        this.channel = await this.connection.createChannel()
+        //const exchange = process.env.RABBIT_BI_EiXCHANGE
+        //await this.channel.assertExchange(exchange, 'direct', { durable: true }).catch(console.error)
+    }
+
+    async emitEvent(queue: string, pattern: string, data: any) {
+        //{"pattern":"whatsapp_test_event","data":{"name":"hsjjk","jjj":"a5456"}}
+        const content = { pattern, data }
+        return this.channel.sendToQueue(queue, Buffer.from(JSON.stringify(content)))
+    }
+}
