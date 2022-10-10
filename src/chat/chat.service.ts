7806bb8f1544038bd0dc0cc017b6ab7e7bcc56d0
vector-rc <fernando.rdcrz@gmail.com>
2022-10-10 17:10:59 -0500

    fixed bug when delete non-existent chat

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -7,12 +7,19 @@ export class ChatService {
     constructor(private sessionService: SessionService) {
     }
 
-    async remove(data:{whatsappNumber:string,chatNumber:string}) {
+    async remove(data: { whatsappNumber: string, chatNumber: string }) {
         const session = this.sessionService.findByWid(data.whatsappNumber)
-        if(!session){
-            throw new HttpException({success:false,message:'No existe session con este numero'},HttpStatus.NOT_ACCEPTABLE)
+        if (!session) {
+            throw new HttpException({ success: false, message: 'No existe session con este numero' }, HttpStatus.NOT_ACCEPTABLE)
         }
-        return await session.whatsapp.deleteChat(data.chatNumber+'@c.us')
+        try {
+            await session.whatsapp.deleteChat(data.chatNumber + '@c.us')
+            return true
+        } catch (error) {
+            console.log('ERROR AL ELIMINAR EL CHAT', error);
+            return false
+
+        }
     }
 
 }
